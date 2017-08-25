8dfe29bdb423160c2a5d93596099b041c9477517
Peter Bui <pbui@nd.edu>
2017-08-25 00:19:08 -0400

    dredd.quiz: check either str or unicode

--- dredd/quiz.py
+++ dredd/quiz.py
@@ -47,7 +47,7 @@ class Quiz(object):
     def evaluate_multiple(self, question, responses):
         result = 0
 
-        if isinstance(responses, str):
+        if isinstance(responses, str) or isinstance(responses, unicode):
             responses = [responses]
 
         answers, value = self.answers[question]
@@ -60,7 +60,7 @@ class Quiz(object):
     def evaluate_blank(self, question, responses):
         result = 0
 
-        if isinstance(responses, str):
+        if isinstance(responses, str) or isinstance(responses, unicode):
             responses = [responses]
 
         for response, answers in zip(responses, self.answers[question]):
