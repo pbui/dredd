c21751424930570068d6f1fa8adc98682f706dc5
Peter Bui <pbui@nd.edu>
2020-05-06 18:23:06 -0400

    sandbox: update Docker image, add support for timeout

--- scripts/sandbox.sh
+++ scripts/sandbox.sh
@@ -11,17 +11,15 @@ SOURCE_PATH=${3}
 INPUT_PATH=${4}
 OUTPUT_PATH=${5}
 
-#IMAGE=pbui/dredd-code:20171108
-#IMAGE=pbui/dredd-code:20171218
-#IMAGE=pbui/dredd-code:20180806
-#IMAGE=pbui/dredd-code:20180813
-#IMAGE=pbui/dredd-code:20180829
-#IMAGE=pbui/dredd-code:20190516
-#IMAGE=pbui/dredd-code:20190819
-IMAGE=pbui/dredd-code:20190830
+#IMAGE=pbui/dredd-code:20191004
+IMAGE=pbui/dredd-code:20200506
 
 if [ ! -d "$SANDBOX_PATH" ]; then
     mkdir -p "$SANDBOX_PATH"
+fi
+
+if [ -r $(dirname $INPUT_PATH)/timeout ]; then
+    TIMEOUT="-t $(cat $(dirname $INPUT_PATH)/timeout)"
 fi
 
 cp -f "$RUNNER_PATH" "$SANDBOX_PATH"
@@ -29,6 +27,10 @@ cp -f "$SOURCE_PATH" "$SANDBOX_PATH" > /dev/null 2>&1
 cp -f "$INPUT_PATH" "$SANDBOX_PATH"  > /dev/null 2>&1
 cp -f "$OUTPUT_PATH" "$SANDBOX_PATH" > /dev/null 2>&1
 
-docker run -e DEBUG=${DEBUG:-0} -i -w /sandbox -v "$SANDBOX_PATH":/sandbox $IMAGE ./$(basename $RUNNER_PATH) $(basename $SOURCE_PATH) $(basename $INPUT_PATH) $(basename $OUTPUT_PATH)
+docker run -e DEBUG=${DEBUG:-0} -i -w /sandbox -v "$SANDBOX_PATH":/sandbox $IMAGE \
+    ./$(basename $RUNNER_PATH) $TIMEOUT \
+        $(basename $SOURCE_PATH) \
+        $(basename $INPUT_PATH) \
+        $(basename $OUTPUT_PATH)
 
 # vim: set sts=4 sw=4 ts=8 expandtab ft=sh:
