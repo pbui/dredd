0a12b1046e14bc5543de96e177fcfbbca197fc71
Peter Bui <pbui@nd.edu>
2021-01-22 13:17:25 -0500

    dredd.quiz: return total number of possible points and the status of the quiz
    Status is success if number of responses matches number of answers AND
    the score matches the possible points.

--- dredd/quiz.py
+++ dredd/quiz.py
@@ -22,29 +22,34 @@ class Quiz(object):
     def evaluate(self, responses):
         ''' Evaluate Quiz responses '''
         result = {}
+        points = {}
         for question, response in responses.items():
             try:
                 if self.questions[question]['type'] == 'single':
-                    result[question] = self.evaluate_single(question, response)
+                    result[question], points[question] = self.evaluate_single(question, response)
                 elif self.questions[question]['type'] == 'multiple':
-                    result[question] = self.evaluate_multiple(question, response)
+                    result[question], points[question] = self.evaluate_multiple(question, response)
                 elif self.questions[question]['type'] == 'order':
-                    result[question] = self.evaluate_order(question, response)
+                    result[question], points[question] = self.evaluate_order(question, response)
                 elif self.questions[question]['type'] == 'blank':
-                    result[question] = self.evaluate_blank(question, response)
+                    result[question], points[question] = self.evaluate_blank(question, response)
                 else:
-                    result[question] = 0
+                    result[question], points[question] = 0, 0
             except KeyError:
-                result[question] = 0
+                result[question], points[question] = 0, 0
 
-        result['score'] = float('{:0.4f}'.format(sum(result.values())))
+        result['score']  = float('{:0.4f}'.format(sum(result.values())))
+        result['value']  = float('{:0.4f}'.format(sum(points.values())))
+        result['status'] = 0 if len(responses) == len(self.answers) and result['score'] >= result['value'] else 1
         return result
 
     def evaluate_single(self, question, response):
+        values = []
         for answer, value in self.answers[question]:
+            values.append(value)
             if response == answer:
-                return value
-        return 0
+                return value, max(values)
+        return 0, max(values)
 
     def evaluate_multiple(self, question, responses):
         if isinstance(responses, str):
@@ -55,24 +60,28 @@ class Quiz(object):
         aset    = set(answers)
         missing = 1.0*len(aset.difference(rset)) / len(answers)
         extra   = 0.5*len(rset.difference(aset)) / len(answers)
-        return (1.0 - missing - extra)*value
+        return (1.0 - missing - extra)*value, value
 
     def evaluate_order(self, question, responses):
         answers, value = self.answers[question]
         ratio = sum(1 for r, a in zip(responses, answers) if r == a) / len(answers)
-        return ratio * value
+        return ratio * value, value
 
     def evaluate_blank(self, question, responses):
         result = 0
+        points = 0
 
         if isinstance(responses, str):
             responses = [responses]
 
         for response, answers in zip(responses, self.answers[question]):
+            values = []
             for answer, value in answers:
+                values.append(value)
                 if answer.lower() == response.lower():
                     result += value
                     break
-        return result
+            points += max(values)
+        return result, points
 
 # vim: set sts=4 sw=4 ts=8 expandtab ft=python:
