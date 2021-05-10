b54a281e31ca0475edf81f5e1c323eadb00c065d
Mike E <me@michaeleisemann.com>
2021-05-09 20:21:58 -0400

    Add Swift support

--- tests/test_code_echo.sh
+++ tests/test_code_echo.sh
@@ -193,6 +193,19 @@ echo -n "Testing Haskell ... "
 curl -F source=@$SOURCE localhost:9206/code/test-echo
 rm -f $SOURCE
 
+# Swift
+
+SOURCE=$(mktemp -t dredd_XXXXXXX.swift)
+cat > $SOURCE <<EOF
+while let line = readLine() {
+    print(line)
+}
+EOF
+echo
+echo -n "Testing Swift ... "
+curl -F source=@$SOURCE localhost:9206/code/test-echo
+rm -rf $SOURCE
+
 
 # # Brainfuck
 # 
